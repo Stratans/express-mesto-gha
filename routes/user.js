67313ff6f9f5db56feec35d0f6424b0aca54d975
Dmitry Stratan <streitan@yandex.ru>
2023-06-22 18:18:28 +0300

    fix errors joi

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+
 const {
   getUsers,
   getUserInfo,
@@ -9,8 +11,21 @@ const {
 
 router.get('/', getUsers);
 router.get('/me', getUserInfo);
-router.get('/:_id', getUserById);
-router.patch('/me', updateProfile);
-router.patch('/me/avatar', updateAvatar);
+router.get('/:_id', celebrate({
+  params: Joi.object().keys({
+    _id: Joi.string().length(24).required(),
+  }),
+}), getUserById);
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+}), updateProfile);
+router.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().required().regex(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/),
+  }),
+}), updateAvatar);
 
 module.exports = router;
