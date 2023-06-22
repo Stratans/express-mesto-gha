acdd42a330834efcdb6b1813fde029fd975a643a
Dmitry Stratan <streitan@yandex.ru>
2023-06-22 19:04:27 +0300

    fix errors joi v5

--- models/user.js
+++ models/user.js
@@ -55,7 +55,6 @@ userSchema.statics.findUserByCredentials = function (email, password) {
           if (!matched) {
             return Promise.reject(new Error('Неправильные почта или пароль'));
           }
-
           return user; // теперь user доступен
         });
     });
