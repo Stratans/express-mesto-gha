055e392fcc9aa41db6c5ceed53c81e6077046984
Dmitry Stratan <streitan@yandex.ru>
2023-06-23 07:36:17 +0300

    fix bugs first iteration 1.1

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,11 +1,10 @@
 const jwt = require('jsonwebtoken');
+const UnAuthorizedError = require('../errors/unAuthorizedError');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
+    return next(new UnAuthorizedError('Приехали! Необходима авторизация!'));
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -14,9 +13,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, 'secret-key');
   } catch (err) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
+    return next(new UnAuthorizedError('Приехали! Необходима авторизация!'));
   }
 
   req.user = payload; // записываем пейлоуд в объект запроса
